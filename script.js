fde167b3ea116e516d2d124a36141d9100e7db89
dreaMykj <gabrielgreghi2018@gmail.com>
2024-04-12 12:01:22 -0300

    Atividade concluída

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-console.log ("Olá usuário, \nQual o seu nome?");
+alert ("Olá usuário");
 
 let nome = prompt("Qual o seu nome?");
-console.log(nome);
 
 let sobrenome = prompt("Qual o seu sobrenome?");
-console.log(sobrenome);
 
 let resposta = alert("Seu nome");
-console.log(nome, sobrenome);
 alert(nome + " " + sobrenome);
 
 let resposta2 = alert("Nome de catálogo")
-console.log(sobrenome.toUpperCase(), nome);
 alert(sobrenome.toUpperCase() + " , " + nome);
